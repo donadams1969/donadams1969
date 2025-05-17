e1f263e8f6e2ed0616ce4467ab65ae7f8a7c40fd
Donny Adams <donny@18fu.ai>
2025-05-17 03:06:36 -0700

    Update MyContract.sol

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    string public message = "Hello, Remix!";
+
+    function setMessage(string calldata _msg) public {
+        message = _msg;
+    }
+}
 
